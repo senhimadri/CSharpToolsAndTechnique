6a19c6b57cd2baa37517a5756ad8b9eb8782244d
senhimadri <senhimadri0176@gmail.com>
2023-08-08 15:06:54 +0600

    With Statment

---
+++ CSharpToolsAndTechnique/ProblemStatment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToolsAndTechnique
+{
+    public class ProblemStatment
+    {
+
+        public int WithStatment()
+        {
+            var Banana = new
+            {
+                Name = "Banana",
+                Price = 120
+            };
+
+            var BananaonDiscount = Banana with
+            {
+                Price = 99
+            };
+
+            return 1;
+        }
+    }
+}
--- CSharpToolsAndTechnique/Program.cs
+++ CSharpToolsAndTechnique/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CSharpToolsAndTechnique;
+
 Console.WriteLine("Hello, World!");
+
+ProblemStatment PS = new ProblemStatment();
+
+
+
+var SS = PS.WithStatment();
