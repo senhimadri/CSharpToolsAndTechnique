8dffe7735c53a451b7334dba54c0d95b43438817
senhimadri <senhimadri.dev@gmail.com>
2023-08-10 14:40:47 +0600

    Extension Methods in c#

---
+++ CSharpToolsAndTechnique/ExtensionMethods/ExecutionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToolsAndTechnique.ExtensionMethods
+{
+    public static class ExecutionHelper
+    {
+        public static string StringSubstruction(this String _thisStrin, int Length)
+        {
+            var Res = _thisStrin.Substring(0, Length);
+            return Res;
+        }
+
+    }
+}
--- CSharpToolsAndTechnique/Program.cs
+++ CSharpToolsAndTechnique/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpToolsAndTechnique;
+using CSharpToolsAndTechnique.ExtensionMethods;
 
 Console.WriteLine("Hello, World!");
 
 ProblemStatment PS = new ProblemStatment();
 
 
-
-//var SS = PS.WithStatment();
-
 PS.RecordStatmentinC();
+
+string Executio = "Himadri Sen Apu";
+
+
+var A =Executio.StringSubstruction(5);
+
+Console.WriteLine(Executio);
+
+Console.ReadLine();
