eda5852f1b1285c9a0aa3b35658732c7fbd382ae
senhimadri <senhimadri.dev@gmail.com>
2023-10-04 17:51:31 +0600

    Required Key Word

---
+++ CSharpToolsAndTechnique/RequirdeKeywords/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToolsAndTechnique.RequirdeKeywords;
+
+public class Person
+{
+    public Guid Id { get; set; }= Guid.NewGuid();
+    public required string FullName { get; init; }
+
+
+}
+
+public class RequiredKeyImplementation
+{
+    public void Implementation()
+    {
+        var person = new Person() { FullName=""};
+    }
+}
